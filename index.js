a1e474ae43d2a6f1ad108eb4a6b90a5bada7ba8f
&y <andes.collab@gmail.com>
2015-07-06 23:28:43 -0400

    mentions hilary in readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-module.exports = [
-    require('./src/Blueprint.js'),
-    require('./src/exceptions.js'),
-    require('./src/id.js'),
-    require('./src/is.js'),    
-    require('./src/utils.js')
-];
+module.exports = {
+    Blueprint: require('./src/Blueprint.js'),
+    exceptions: require('./src/exceptions.js'),
+    id: require('./src/id.js'),
+    is: require('./src/is.js'),
+    utils: require('./src/utils.js')    
+};
