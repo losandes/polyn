a6b1acecd0ba9cf08d54cb9b14123b2ff8c012b9
&y <andes.collab@gmail.com>
2016-11-20 01:22:24 -0500

    easier minification swap for tests

--- build-tasks/test-browsers.js
+++ build-tasks/test-browsers.js
@@ -2,6 +2,24 @@
 module.exports = function (grunt) {
     'use strict';
 
+    var makeFileArray = function (minified) {
+        var polyn = minified ? 'release/polyn.min.js' : 'release/polyn.js';
+        return [
+            'tests/browser-setup.js',
+            // polyn
+            polyn,
+            // specs
+            'tests/specs/async.fixture.js',
+            'tests/specs/Blueprint.fixture.js',
+            'tests/specs/Exception.fixture.js',
+            'tests/specs/id.fixture.js',
+            'tests/specs/Immutable.fixture.js',
+            'tests/specs/is.fixture.js',
+            // runner
+            'tests/browser-bootstrapper.js'
+        ];
+    };
+
     grunt.loadNpmTasks('grunt-mocha'); // browser
     grunt.loadNpmTasks('grunt-karma');
 
@@ -11,20 +29,7 @@ module.exports = function (grunt) {
             // see http://karma-runner.github.io/0.8/config/configuration-file.html
             basePath: './',
             frameworks: ['mocha', 'chai'],
-            files: [
-                'tests/browser-setup.js',
-                // polyn
-                'release/polyn.js',
-                // specs
-                'tests/specs/async.fixture.js',
-                'tests/specs/Blueprint.fixture.js',
-                'tests/specs/Exception.fixture.js',
-                'tests/specs/id.fixture.js',
-                'tests/specs/Immutable.fixture.js',
-                'tests/specs/is.fixture.js',
-                // runner
-                'tests/browser-bootstrapper.js'
-            ],
+            files: makeFileArray(true),
             reporters: ['nyan'],
             reportSlowerThan: 2000,
             singleRun: true
